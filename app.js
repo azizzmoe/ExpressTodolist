4f262f7961ed2f4fad889d15e152cb700222ec23
azizzmoe <aziznageeye9@gmail.com>
2023-05-02 23:32:52 +0300

    switched all func to async

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/todolist');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/todolist");
 const Task = require("./DB/Task");
 
 const app = express();
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-
-async function init() {
-  // const arr = [{ name: 'Do Home Work' }, { name: 'Clean Set-Up' }];
-  // Task.insertMany(arr);
-  const items = await Task.find({})
-  return items
-}
-
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   const today = new Date();
   let options = {
     weekday: "long",
@@ -29,28 +20,31 @@ app.get("/", (req, res) => {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  init().then((fountItems) => {
-    
-    res.render("list", { listTitle: day, newlistItems: fountItems });
-  })
-
-});
-
-app.post("/", (req, res) => {
-  let item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workList.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+  try {
+    let items = await Task.find({});
+    res.render("list", { listTitle: day, newlistItems: items });
+  } catch (error) {
+    console.error("Error while getting data:", error);
+    res.status(500).send("Error while getting data");
   }
 });
 
-// app.get("/work", (req, res) => {
-//   res.render("list", { listTitle: "Work List", newlistItems: workList });
-// });
+app.post("/", async (req, res) => {
+  let taskName = req.body.newItem;
+
+  const task = new Task({
+    name: taskName,
+  });
+
+  try {
+    await task.save();
+    console.log("Item saved successfully");
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error while getting data:", error);
+    res.status(500).send("Error while getting data");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is running from port 3000");
